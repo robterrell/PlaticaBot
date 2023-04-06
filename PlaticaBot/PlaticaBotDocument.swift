525b50c00569017c68a814ae7569355a59153348
Rob Terrell <robterrell@gmail.com>
2023-04-06 16:50:53 +0000

    Implemented PlaticaBotDocument.

--- PlaticaBot/PlaticaBotDocument.swift
+++ PlaticaBot/PlaticaBotDocument.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import UniformTypeIdentifiers
+import SwiftUI
+
+class PlaticaBotDocument: FileDocument {
+    @ObservedObject var store: InteractionStorage 
+    
+    static var readableContentTypes: [UTType] { [.platicaBotJSON] }
+    
+    init() {
+        self.store = InteractionStorage()
+    }
+
+    required init(configuration: ReadConfiguration) throws {
+        guard let data = configuration.file.regularFileContents,
+              let decodedData = try? JSONDecoder().decode([Interaction].self, from: data)
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+        self.store = InteractionStorage()
+        self.store.interactions.append(contentsOf: decodedData)
+    }
+
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let encodedData = try JSONEncoder().encode(store.interactions)
+        return FileWrapper(regularFileWithContents: encodedData)
+    }
+    
+}
+
+// Add this extension for your custom UTType
+extension UTType {
+    static var platicaBotJSON: UTType {
+        UTType(exportedAs: "com.platicabot.json")
+    }
+}
